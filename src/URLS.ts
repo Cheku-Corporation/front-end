0607459407917b9237ee69e55de907a6043d505f
v1centebarros <vmabarros@gmail.com>
2022-12-13 18:25:51 +0000

    ADD API Documentation Link

--- src/URLS.ts
+++ src/URLS.ts
@@ -1,5 +1,7 @@
 const HOST =  "http://localhost:8080"
 
+//API DOCUMENTATION
+// https://documenter.getpostman.com/view/13973483/2s8YzMY5S1#c0bfaa9f-cd03-4fb4-b4da-6cd3bd81daa2
 
 export const REGISTER_URL = ( ) =>  `${HOST}/register`
 export const LOGIN_URL = ( ) =>  `${HOST}/login`
